433b914ab497a52ce55291ee1466d50b2fc74944
Chris Rebuck <crebuck.apt@gmail.com>
2021-09-23 17:20:42 -0500

    added button functionality, modified CSS

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  //do not use a dot with this because we are not selecting anything we are just passing in the name of the class
+  overlay.classList.remove("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
